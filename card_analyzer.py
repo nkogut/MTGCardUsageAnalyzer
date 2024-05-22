d21645bf541c8f1a0dfa48d79c4e09146ca52876
nkogut <nkogut4444@gmail.com>
2024-05-22 11:51:14 -0400

    Add gui to make running queries in card_analysis.py more intuitive. Update find_card_prevalence() and display_decks() to return a string to the gui calls instead of printing directly.

--- card_analyzer.py
+++ card_analyzer.py
@@ -5,28 +5,26 @@ from datetime import *
 # with open("Data/double_cards.json", "r") as f:
 #     doubles = json.load(f)
 
+def load_dataset(dataset):
+    with open(dataset, "r") as f:
+        global data
+        data = json.load(f)
 
-DATASET = "full_modern_dataset.json"
-with open(DATASET, "r") as f:
-    data = json.load(f)
-
-def display_deck(decks=None):
+def display_decks(decks=None):
     """
     decks: a list of decks to display. Generate using find_decks()
-    prints each decklist in a readable way
-    returns nothing
+    converts each decklist to a human-readable string
+    returns a string formatted to be readable
     """
     if decks is None:
         decks = []
 
     with open("Data/card_properties.json", "r") as f:
         card_properties = json.load(f)
-    # TODO
-    # sort lands to front
-    # possibly sort by type
+    output = ""
 
     for deck in decks:
-        # VREY TEMPORARY FIX FOR LOTR accent cards - REPLACE IT WITH GIFTED AETHERBORN
+        # VERY TEMPORARY FIX FOR LOTR accent cards - REPLACE IT WITH GIFTED AETHERBORN
         broken_names = ['Troll of Khazad-dÃ»m', "LÃ³rien Revealed"]
         for card in broken_names:
 
@@ -37,21 +35,31 @@ def display_deck(decks=None):
             if card in deck['sideboard']:
                 deck['sideboard']['Gifted Aetherborn'] = deck['sideboard'][card]
                 del deck['sideboard'][card]
-        # troubleshooting end
+        # end of temporary troubleshooting code
 
-        print(deck['player'], deck['url'])
-
-
-
-        maindeck = sorted(deck['maindeck'].keys(), key=lambda x: card_properties[x]['cmc'])
-        sideboard = sorted(deck['sideboard'].keys(), key=lambda x: card_properties[x]['cmc'])
+        output += f"\n {deck['player']} {deck['url']}"
+        try:
+            maindeck = sorted(deck['maindeck'].keys(), key=lambda x: card_properties[x]['cmc'])
+            sideboard = sorted(deck['sideboard'].keys(), key=lambda x: card_properties[x]['cmc'])
+        except KeyError:
+            # Investigating why this happens. If an error occurs, just display the deck without sorting by CMC
+            pass
 
         for card in maindeck:
-            print(f"{deck['maindeck'][card]} {card} - {card_properties[card]['mana_cost']}")
-        print("------ SIDEBOARD ------")
+            try:
+                output += f"\n{deck['maindeck'][card]} {card} - {card_properties[card]['mana_cost']}"
+            except KeyError:
+                output += f"\n{card} - error retrieving CMC or quantity"
+                # Investigating why this happens. If an error occurs, display without CMC or quantity
+        output += "\n------ SIDEBOARD ------"
         for card in sideboard:
-            print(f"{deck['sideboard'][card]} {card} - {card_properties[card]['mana_cost']}")
-        print("------ END OF DECK ------")
+            try:
+                output += f"\n{deck['sideboard'][card]} {card} - {card_properties[card]['mana_cost']}"
+            except KeyError:
+                output += f"\n{card} - error retrieving CMC or quantity"
+                # Investigating why this happens. If an error occurs, display without CMC or quantity
+        output += "\n------ END OF DECK ------"
+    return output
 
 def find_decks(whitelist=[], blacklist=[], player=None, min_date=date(2000, 1, 1), max_date=date(2100, 1, 1),
                search_in=["maindeck", "sideboard"],
@@ -64,9 +72,6 @@ def find_decks(whitelist=[], blacklist=[], player=None, min_date=date(2000, 1, 1
     max_date: most recent decks to consider - use datetme module: date(yyyy, mm, dd)
     search_in: specify to search in maindeck and/or sideboard
     event_type: List of types of MTGO events to consider
-
-
-
     returns list of dicts that represent each deck from the dataset that matches all criteria
     """
 
@@ -105,8 +110,8 @@ def find_decks(whitelist=[], blacklist=[], player=None, min_date=date(2000, 1, 1
                         matched_cards.append(card)
         if len(matched_cards) == len(whitelist) and len(blacklisted_cards) == 0:
             found_decks.append(decklist)
-    print(f'{len(decks_in_period)} decks considered in period')
-    print(f'{len(found_decks)} decks apply, {(len(found_decks) / len(decks_in_period)) * 100:.2f}% of all decks')
+    # print(f'{len(decks_in_period)} decks considered in period')
+    # print(f'{len(found_decks)} decks apply, {(len(found_decks) / len(decks_in_period)) * 100:.2f}% of all decks in dataset')
     return found_decks
 
 
@@ -115,9 +120,9 @@ def find_card_prevalence(sample=None, search_in=["maindeck", "sideboard"]):
     sample: a list of all decklists to consider. Generate using find_decks()
     search_in: specify to search in maindeck/sidebooard
 
-    prints how many copies of each card exist in the sample, as well as % of decks the card is in. Split into maindeck
+    creates string that tells how many copies of each card exist in the sample, as well as % of decks the card is in. Split into maindeck
     on top and sideboard below
-    returns nothing
+    returns a string formatted to be readable
     """
     if sample is None:
         sample = []
@@ -145,22 +150,23 @@ def find_card_prevalence(sample=None, search_in=["maindeck", "sideboard"]):
                 else:
                     prev_dict_side[card] = [1, deck["sideboard"][card]]
 
-    print(f'{len(prev_dict_main)} MD cards')
+
+    output = ""
+    # output += f'{len(prev_dict_main)} Unique maindeck cards found'
     for card, quantity in sorted(prev_dict_main.items(), key=lambda x: x[1][0], reverse=True):
         #returns tuple (card, [# of decks, # of cards])
-
-        print(f"{card} - {quantity[0]} - {((quantity[0] / num_decks) * 100):.2f}% - {(quantity[1] / quantity[0]):.2f} average # played ")
-    print("\n---SIDEBOARD---\n")
-    print(f'{len(prev_dict_side)} SB cards')
+        output += f"\n{card} - {quantity[0]} - {((quantity[0] / num_decks) * 100):.2f}% - {(quantity[1] / quantity[0]):.2f} average # played "
+    output += "\n\n---SIDEBOARD---\n"
     for card, quantity in sorted(prev_dict_side.items(), key=lambda x: x[1][0], reverse=True):
-        print(f"{card} - {quantity[0]} - {((quantity[0] / num_decks) * 100):.2f}% - {(quantity[1] / quantity[0]):.2f} average # played ")
+        output += f"\n{card} - {quantity[0]} - {((quantity[0] / num_decks) * 100):.2f}% - {(quantity[1] / quantity[0]):.2f} average # played "
+    return output
 
 if __name__ == "__main__":
+    load_dataset("Data/full_modern.json")
 
     # Example:
-    find_card_prevalence(find_decks(min_date=date(2023, 12, 4),
+    print(find_card_prevalence(find_decks(min_date=date(2023, 12, 4),
                                     whitelist=["The Rack"],
-                                    event_type=["scheduled"]))
-
+                                    event_type=["scheduled"])))
 
 
---
+++ gui.py
@@ -0,0 +1,157 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter.filedialog import askopenfilename
+from tkcalendar import DateEntry
+import card_analyzer as ca
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+dataset_file = "Data/2024_Decks.json" #TODO Set Default
+ca.load_dataset(dataset_file)
+
+deck_search_params = {"whitelist": [], "blacklist": [], "player": None, "min_date": date(2000, 1, 1),
+                      "max_date": date(2100, 1, 1), "search_in": ["maindeck", "sideboard"],
+                      "event_type": ["league", "scheduled"]}
+
+def choose_file():
+    global dataset_file
+    dataset_file = askopenfilename()
+    ca.load_dataset(dataset_file)
+
+def comma_separated_input_parser(input):
+    if input == "":
+        return []
+    input = input.title()
+    out = input.split(", ")
+    if len(out) > 1:
+        return out
+    else:
+        return input.split(",")
+
+def update_output(text):
+    output_textbox.config(state="normal")
+    output_textbox.insert(1.0, text)
+    output_textbox.config(state="disabled")
+
+def query_decks():
+    # Set which part of deck to search in
+    if not (search_sideboard.get() + search_maindeck.get()) % 2 == 0:
+        if search_maindeck.get() == 0:
+            deck_search_params["search_in"] = ["sideboard"]
+        else:
+            deck_search_params["search_in"] = ["maindeck"]
+    else:
+        deck_search_params["search_in"] = ["maindeck", "sideboard"]
+
+    # Set which types of events to search in
+    if not (search_league.get() + search_scheduled.get()) % 2 == 0:
+        if search_league.get() == 0:
+            deck_search_params["event_type"] = ["scheduled"]
+        else:
+            deck_search_params["event_type"] = ["league"]
+    else:
+        deck_search_params["event_type"] = ["league", "scheduled"]
+
+    # Set the date range
+    deck_search_params["min_date"] = min_date_selector.get_date()
+    deck_search_params["max_date"] = max_date_selector.get_date()
+
+    # Set whitelist/blacklist
+    deck_search_params["whitelist"] = comma_separated_input_parser(whitelist_textbox.get(1.0, "end-1c"))
+    deck_search_params["blacklist"] = comma_separated_input_parser(blacklist_textbox.get(1.0, "end-1c"))
+
+    #Set Player
+    player_input = player_textbox.get(1.0, "end-1c")
+    if player_input == "":
+        deck_search_params["player"] = None
+    else:
+        deck_search_params["player"] = player_input
+
+    return ca.find_decks(*deck_search_params.values())
+
+# Window setup
+root = tk.Tk()
+root.title("MTG Card Usage Analyzer")
+
+# Dataset selection
+file_picker_button = ttk.Button(root, text="Change Dataset", command=choose_file)
+analyze_decks_button = ttk.Button(root, text="Analyze Decks", command=lambda: update_output(ca.find_card_prevalence(query_decks())))
+show_decks_button = ttk.Button(root, text="Show Decks", command=lambda: update_output(ca.display_decks(query_decks())))
+
+# Maindeck/Sideboard selection
+search_maindeck = tk.IntVar(value=1)
+search_sideboard = tk.IntVar(value=1)
+deck_location_menu = tk.Frame(root)
+deck_location_label = tk.Label(deck_location_menu, text="Search in:")
+maindeck_checkbox = ttk.Checkbutton(deck_location_menu, text="Maindeck", variable=search_maindeck)
+sideboard_checkbox = ttk.Checkbutton(deck_location_menu, text="Sideboard", variable=search_sideboard)
+
+deck_location_label.grid(row=0, columnspan=2)
+maindeck_checkbox.grid(row=1, column=0)
+sideboard_checkbox.grid(row=1, column=1)
+
+
+#Event type selection
+search_league = tk.IntVar(value=1)
+search_scheduled = tk.IntVar(value=1)
+event_type_menu = tk.Frame(root)
+event_type_label = tk.Label(event_type_menu, text="Event types:")
+league_checkbox = ttk.Checkbutton(event_type_menu, text="Leagues", variable=search_league)
+scheduled_checkbox = ttk.Checkbutton(event_type_menu, text="Scheduled", variable=search_scheduled)
+
+event_type_label.grid(row=0, columnspan=2)
+league_checkbox.grid(row=1, column=0)
+scheduled_checkbox.grid(row=1, column=1)
+
+
+#Date selection
+date_menu = tk.Frame(root)
+default_start_date = date.today() + relativedelta(months=-6)
+min_date_label = tk.Label(date_menu, text="Start date:")
+max_date_label = tk.Label(date_menu, text="End date:")
+min_date_selector = DateEntry(date_menu, year=default_start_date.year, month=default_start_date.month, day=1)
+max_date_selector = DateEntry(date_menu)
+
+min_date_label.grid(row=0, column=0)
+max_date_label.grid(row=0, column=1)
+min_date_selector.grid(row=1, column=0)
+max_date_selector.grid(row=1, column=1)
+
+
+# Whitelist/Blacklist selection
+text_input_menu = tk.Frame(root)
+whitelist_label = tk.Label(text_input_menu, text="Decks including:")
+blacklist_label = tk.Label(text_input_menu, text="Excluding decks with:")
+player_label = tk.Label(text_input_menu, text="Player (up to 1)")
+whitelist_instruction_label = tk.Label(text_input_menu, text="Enter as 'card a,card b,card c'")
+whitelist_textbox = tk.Text(text_input_menu, height=1, width=25)
+blacklist_textbox = tk.Text(text_input_menu, height=1, width=25)
+player_textbox = tk.Text(text_input_menu, height=1, width=15)
+
+whitelist_instruction_label.grid(row=0, column=1)
+whitelist_label.grid(row=1, column=0)
+whitelist_textbox.grid(row=1, column=1)
+blacklist_label.grid(row=2, column=0)
+blacklist_textbox.grid(row=2, column=1)
+player_label.grid(row=3, column=0)
+player_textbox.grid(row=3, column=1)
+
+# Output
+output_textbox = scrolledtext.ScrolledText(root, state="disabled", width=80, height=10, wrap=tk.WORD)
+
+
+# Populate window
+file_picker_button.grid(row=0, column=0)
+date_menu.grid(row=0, column=1)
+deck_location_menu.grid(row=1, column=0)
+event_type_menu.grid(row=1, column=1)
+text_input_menu.grid(row=2, columnspan=2)
+analyze_decks_button.grid(row=3, column=0)
+show_decks_button.grid(row=3, column=1)
+output_textbox.grid(row=4, columnspan=2)
+
+
+# Draw window
+mainframe = ttk.Frame(root)
+root.mainloop()
